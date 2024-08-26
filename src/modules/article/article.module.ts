dad7ffb9277d0c953a4e7e89989c82565192d5c4
punkmohamed <punkmohamed22@gmail.com>
2024-08-26 22:07:00 +0200

    image uploads and tokens

--- src/modules/article/article.module.ts
+++ src/modules/article/article.module.ts
@@ -6,9 +6,20 @@ import { Tag, TagSchema } from 'src/core/schema/tags.chema';
 import { User, UserSchema } from 'src/core/schema/user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MulterModule } from '@nestjs/platform-express';
+import { extname } from 'path';
+import * as multer from 'multer';
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './upload');
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + extname(file.originalname));
+  },
+});
 @Module({
   imports: [MulterModule.register({
-    dest: "./upload"
+    storage
   }), MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }, { name: Tag.name, schema: TagSchema }, { name: User.name, schema: UserSchema }])],
   providers: [ArticleService],
   controllers: [ArticleController]
